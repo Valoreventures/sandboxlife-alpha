108bf3b7097451eb5a5c570393a670af0acef64a
Meghashyam Kodmad <shyam.kodmad@gmail.com>
2024-03-13 23:49:08 +0530

    added cors headers

--- app/api/users/login.tsx
+++ app/api/users/login.tsx
@@ -1,7 +1,7 @@
 
 export async function POST(request) {
   console.log(request, 'req');
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/userss/login`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/users/login`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',

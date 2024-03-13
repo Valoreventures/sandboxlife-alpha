53a27655d14c992d825d6c4e805b61340622b68f
Meghashyam Kodmad <shyam.kodmad@gmail.com>
2024-03-13 23:23:37 +0530

    added api route

---
+++ app/api/users/login.tsx
@@ -0,0 +1,16 @@
+
+export async function POST(request) {
+  console.log(request, 'req');
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/users/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      // 'API-Key': process.env.DATA_API_KEY!,
+    },
+    body: request.body,
+  })
+ 
+  const data = await res.json()
+ 
+  return Response.json(data)
+}
